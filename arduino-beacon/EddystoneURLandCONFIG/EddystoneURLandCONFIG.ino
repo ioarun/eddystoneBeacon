4ee26da7ce2ce8ba119aaac5972c6455892e36da
Arun Kumar <arun271@users.noreply.github.com>
2016-04-14 09:44:01 +0530

    Update EddystoneURLandCONFIG.ino

--- arduino-beacon/EddystoneURLandCONFIG/EddystoneURLandCONFIG.ino
+++ arduino-beacon/EddystoneURLandCONFIG/EddystoneURLandCONFIG.ino
@@ -1,4 +1,3 @@
-#include "Thread.h"
 #include <EddystoneBeacon.h>
 #include <SPI.h>
 #include "myNRF8001.h"
@@ -29,7 +28,7 @@ void setup(){
  
   pinMode(button,INPUT);
   myThread.onRun(Callback);
-  eddystoneBeacon.begin(-18,"w.google.c");
+  eddystoneBeacon.begin(-18,"http://www.google.com");
   
  Serial.begin(57600);
 }
@@ -72,52 +71,6 @@ void loop(){
  
   ble_do_events();
 }
-/*
-  String read_data_new = "";
-  for(int i=0 ; i < len+1 ; i ++){
-    read_data_new.concat(read_data[i]);   // oh yeaaa! but stops the serial communication!
- }
-
-*/
-
- //String newURL = String("w."+read_data_new+".c");  //does task of concatenating two strings
- /*
- String newURL = "";
- newURL.concat("w.");
- newURL.concat(read_data_new);
- newURL.concat(".c");
-*/
-/*
- //works fine
- char testArr[9];
- 
- testArr[0] = 'w';
- testArr[1] = '.';
- testArr[2] = read_data[0];
- testArr[3] = read_data[1];
- testArr[4] = read_data[2];
- testArr[5] = read_data[3];
- testArr[6] = '.';
- testArr[7] = 'c';
- testArr[8] = '\0';
-
- const char* testPtr = testArr;
-  Serial.println(testPtr);
-  delay(1000);
-*/
-  /*
- for(int i=0 ; i < len+1 ; i ++){
-    Serial.print(read_data[i]);
- }
-  delay(1000);
-  */
-  
-  //ORIGINAL SIZE OF THE DATA IN read_data IS CHAR OF SIZE LEN. Eg. if we send 4 characters, 
-  //the value of LEN shows 5. Maybe due to \0. ( All this when using Simple Chat Application )
-
-  //char ptr[len+5];
-  //char *ptr;
-  //ptr = (char*)malloc(sizeof(char)*(9));   // original size from above was not len it was len+1
   ptr = (char*)malloc(len+4);
   ptr[0] = 'w';
   ptr[1] = '.';
@@ -129,19 +82,6 @@ void loop(){
   ptr[len+1] = '.';
   ptr[len+2] = 'c';
   ptr[len+3] = '\0';
-  
-  
-   /*
-   for(int i=0 ; i < len+5 ; i ++){
-    Serial.print(ptr[i]);
- }
- */
- 
-  //free(ptr);
-
- //casting from string to const char* is causing problem
-  //const char *ptr = (const char*)newURL;  ///note casting ?
-  //const char *ptr = &read_data;
  
   //REset again 
   digitalWrite(reset_pin, HIGH);
